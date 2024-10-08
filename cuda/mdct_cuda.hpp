5feed9956845630fe633005b472e335112b7ef32
Norman Anderson <147473940+anormananderson@users.noreply.github.com>
2024-10-08 02:34:01 +0000

    Prevent cpp name mangling

--- cuda/mdct_cuda.hpp
+++ cuda/mdct_cuda.hpp
@@ -6,6 +6,15 @@
 #define CUDA_KERNEL
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 CUDA_KERNEL void doPreRotation(float *input, float *output, int N);
+
+#ifdef __cplusplus
+}
+#endif
+
 
 void printCudaVersion();
